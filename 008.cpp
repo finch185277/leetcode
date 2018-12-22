7c6a2449bf8a76369ee75dee900dd9a6d173967d
finch185277 <59487andy@gmail.com>
2018-12-23 00:38:30 +0800

    Add 008.cpp

---
+++ 008.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+  inline bool check_valid_char(char &c) {
+    if (c == ' ')
+      return true;
+    else if (c == '-' || c == '+')
+      return true;
+    else if (c >= '0' && c <= '9')
+      return true;
+    else
+      return false;
+  }
+  int myAtoi(string str) {
+    bool pro_flag = false, neg_flag = false;
+    int lb = pow(-2, 31), ub = pow(2, 31) - 1;
+    double ret = 0;
+    std::vector<int> num_vec;
+    for (int i = 0; i < str.size(); i++) {
+      char c = str[i];
+      if (!pro_flag) {
+        if (!check_valid_char(c))
+          return 0;
+        else {
+          if (c == ' ')
+            continue;
+          else if (c == '+')
+            pro_flag = true;
+          else if (c == '-') {
+            neg_flag = true;
+            pro_flag = true;
+          } else if (c >= '0' && c <= '9') {
+            num_vec.push_back(c - '0');
+            pro_flag = true;
+          }
+        }
+      } else {
+        if (c >= '0' && c <= '9') {
+          num_vec.push_back(c - '0');
+        } else
+          break;
+      }
+    }
+    for (int i = 0; i < num_vec.size(); i++) {
+      ret += num_vec.at(i);
+      ret *= 10;
+    }
+    ret /= 10;
+
+    if (neg_flag)
+      ret = -ret;
+
+    if (ret < lb)
+      return lb;
+    else if (ret > ub)
+      return ub;
+    else
+      return ret;
+  }
+};
