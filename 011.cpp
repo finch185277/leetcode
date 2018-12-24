4546d7d109a2fd3992aca155eec67aa572d45e51
finch185277 <59487andy@gmail.com>
2018-12-24 14:34:49 +0800

    Add 011.cpp

---
+++ 011.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  int maxArea(vector<int> &height) {
+    if (height.size() < 2)
+      return 0;
+    int ret = 0, lb = 0, hb = height.size() - 1;
+    while (lb < hb) {
+      int tmp_area = (hb - lb) * std::min(height.at(lb), height.at(hb));
+      ret = std::max(ret, tmp_area);
+      if (height.at(lb) < height.at(hb))
+        lb++;
+      else
+        hb--;
+    }
+    return ret;
+  }
+};
