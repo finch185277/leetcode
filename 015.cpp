7b7077928d398c1d551950b0ba58b4a069a08019
finch185277 <59487andy@gmail.com>
2018-12-25 18:31:44 +0800

    Add 015.cpp

---
+++ 015.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+  vector<vector<int>> threeSum(vector<int> &nums) {
+    std::vector<std::vector<int>> ret;
+    if (nums.size() < 3)
+      return ret;
+    std::sort(nums.begin(), nums.end());
+    bool first_flag = true;
+    for (int i = 0; i < nums.size() - 1; i++) {
+      if (first_flag)
+        first_flag = false;
+      else if (nums.at(i) == nums.at(i - 1)) // skip duplicate
+        continue;
+
+      int lb = i + 1, hb = nums.size() - 1;
+      while (lb < hb) { // double ptr
+        if (nums.at(i) + nums.at(lb) + nums.at(hb) == 0) {
+          std::vector<int> tmp;
+          tmp.push_back(nums.at(i));
+          tmp.push_back(nums.at(lb++));
+          tmp.push_back(nums.at(hb--));
+          if (ret.empty())
+            ret.push_back(tmp);
+          else if (tmp != ret.back()) // skip duplicate
+            ret.push_back(tmp);
+        } else if (nums.at(i) + nums.at(lb) + nums.at(hb) > 0)
+          hb--;
+        else
+          lb++;
+      }
+    }
+    return ret;
+  }
+};
