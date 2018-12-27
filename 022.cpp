819eb829b587c374ddbb8c549d49fbe7a5e6d965
finch185277 <59487andy@gmail.com>
2018-12-27 19:15:07 +0800

    Update 022.cpp

--- 022.cpp
+++ 022.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-  inline bool isValid(string &s) {
-    std::stack<char> match_stack;
-    for (int i = 0; i < s.size(); i++) {
-      if (match_stack.empty() || s[i] == '(')
-        match_stack.push(s[i]);
-      else if (s[i] == ')') {
-        if (match_stack.top() == '(')
-          match_stack.pop();
-        else
-          return false;
-      } else
-        return false;
-    }
-    if (match_stack.empty())
-      return true;
-    else
-      return false;
-  }
   vector<string> generateParenthesis(int n) {
     std::vector<std::string> ret;
-    std::vector<char> recur;
-    for (int i = 0; i < n; i++) {
-      recur.insert(recur.begin(), '(');
-      recur.push_back(')');
+    if (n == 0)
+      ret.push_back("");
+    else {
+      for (int i = 0; i < n; i++)
+        for (std::string left : generateParenthesis(i))
+          for (std::string right : generateParenthesis(n - 1 - i))
+            ret.push_back("(" + left + ")" + right);
     }
-    do {
-      std::string str(recur.begin(), recur.end());
-      if (isValid(str))
-        ret.push_back(str);
-    } while (std::next_permutation(recur.begin(), recur.end()));
     return ret;
   }
 };
