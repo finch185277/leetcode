ff30be737171b8c6bd6554ebd85efec8d23df6b1
finch185277 <59487andy@gmail.com>
2018-12-30 23:49:59 +0800

    Update 030.cpp

--- 030.cpp
+++ 030.cpp
@@ -4,17 +4,24 @@ public:
     std::vector<int> ret;
     if (s.empty() || words.empty())
       return ret;
-    std::sort(words.begin(), words.end());
-    do {
-      std::string target;
-      for (std::string w : words)
-        target += w;
-      int target_len = target.size();
-      for (int i = 0; i < s.size() - target_len + 1; i++) {
-        if (s.substr(i, target_len) == target)
-          ret.push_back(i);
+    int words_count = words.size(), word_size = words.at(0).size();
+    std::unordered_map<std::string, int> words_map;
+    for (std::string w : words)
+      words_map[w]++;
+    for (int i = 0; i <= (int)s.size() - words_count * word_size; i++) {
+      std::unordered_map<std::string, int> cur_map;
+      int count = 0;
+      for (; count < words_count; count++) {
+        string target = s.substr(i + count * word_size, word_size);
+        if (words_map.find(target) == words_map.end())
+          break;
+        cur_map[target]++;
+        if (cur_map.at(target) > words_map.at(target))
+          break;
       }
-    } while (std::next_permutation(words.begin(), words.end()));
+      if (count == words_count)
+        ret.push_back(i);
+    }
     return ret;
   }
 };
