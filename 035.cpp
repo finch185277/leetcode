f3ed5c7b858f94c7693fd4867ae883d936cc580c
finch185277 <59487andy@gmail.com>
2019-01-04 21:22:56 +0800

    Update 035.cpp
    O(n) human-like

--- 035.cpp
+++ 035.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
   int searchInsert(vector<int> &nums, int target) {
-    int lb = 0, hb = nums.size() - 1;
-    if (target < nums.front())
-      return 0;
-    else if (target > nums.back())
-      return nums.size();
-    while (lb < hb) {
-      int mid = (lb + hb) / 2;
-      if (nums.at(mid) == target)
-        return mid;
-      else if (nums.at(mid) < target)
-        lb = mid + 1;
-      else
-        hb = mid;
-    }
-    return hb;
+    for (int i = 0; i < nums.size(); i++)
+      if (nums.at(i) >= target)
+        return i;
+    return nums.size();
   }
 };
