ce194b563fc3b175b162c5d6d89c0c55859cedbd
finch185277 <59487andy@gmail.com>
2019-01-05 12:40:09 +0800

    Update 035.cpp

--- 035.cpp
+++ 035.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
   int searchInsert(vector<int> &nums, int target) {
-    for (int i = 0; i < nums.size(); i++)
-      if (nums.at(i) >= target)
-        return i;
-    return nums.size();
+    return std::lower_bound(nums.begin(), nums.end(), target) - nums.begin();
   }
 };
