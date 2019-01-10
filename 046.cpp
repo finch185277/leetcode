be4d27d3bbdc02ef198d2bfa7d7e43afa6b7cdda
finch185277 <59487andy@gmail.com>
2019-01-10 23:41:34 +0800

    Add 046.cpp

---
+++ 046.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  vector<vector<int>> permute(vector<int> &nums) {
+    std::sort(nums.begin(), nums.end());
+    std::vector<std::vector<int>> ret;
+    do {
+      ret.push_back(nums);
+    } while (std::next_permutation(nums.begin(), nums.end()));
+    return ret;
+  }
+};
