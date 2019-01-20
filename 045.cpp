8e8508c081496e42cec6d856e79a38c5959bd9bc
finch185277 <59487andy@gmail.com>
2019-01-20 22:57:33 +0800

    Add 045.cpp

---
+++ 045.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public: // by @jianchao-li
+  int jump(vector<int> &nums) {
+    int step = 0, start = 0, end = 0;
+    while (end < nums.size() - 1) {
+      step++;
+      int maxend = end + 1;
+      for (int i = start; i <= end; i++) {
+        if (i + nums.at(i) >= nums.size() - 1)
+          return step;
+        maxend = max(maxend, i + nums.at(i));
+      }
+      start = end + 1;
+      end = maxend;
+    }
+    return step;
+  }
+};
