cb2bdf5e2ef142300977a3c56c65fac102962386
finch185277 <59487andy@gmail.com>
2019-01-21 17:46:45 +0800

    Update 048.cpp

--- 048.cpp
+++ 048.cpp
@@ -3,17 +3,17 @@ public:
   void rotate(vector<vector<int>> &matrix) {
     for (int i = 0; i < matrix.size() / 2 + matrix.size() % 2; i++) {
       for (int j = 0; j < matrix.size() / 2; j++) {
-        std::vector<int> tmp(4);
+        std::vector<int> cache(4);
         int row = i;
         int col = j;
         for (int k = 0; k < 4; k++) {
-          tmp.at(k) = matrix.at(row).at(col);
+          cache.at(k) = matrix.at(row).at(col);
           int x = row;
           row = col;
           col = matrix.size() - 1 - x;
         }
         for (int k = 0; k < 4; k++) {
-          matrix.at(row).at(col) = tmp.at((k + 3) % 4);
+          matrix.at(row).at(col) = cache.at((k + 3) % 4);
           int x = row;
           row = col;
           col = matrix.size() - 1 - x;
