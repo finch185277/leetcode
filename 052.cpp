49c26cc4f55b55ad64e49c9a22ad60d52857d74c
finch185277 <59487andy@gmail.com>
2019-01-23 08:49:43 +0800

    Update 052.cpp
    Using bool to stand for queen,
    still 4 ms.

--- 052.cpp
+++ 052.cpp
@@ -1,19 +1,19 @@
 class Solution {
 private:
-  bool is_valid(const std::vector<std::string> &nqueens, const int row,
+  bool is_valid(const std::vector<std::vector<bool>> &nqueens, const int row,
                 const int col, const int &n) {
     for (int i = 0; i < row; i++)
-      if (nqueens.at(i).at(col) == 'Q')
+      if (nqueens.at(i).at(col))
         return false;
     for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
-      if (nqueens.at(i).at(j) == 'Q')
+      if (nqueens.at(i).at(j))
         return false;
     for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
-      if (nqueens.at(i).at(j) == 'Q')
+      if (nqueens.at(i).at(j))
         return false;
     return true;
   }
-  void solve(int &count, std::vector<std::string> &nqueens, const int row,
+  void solve(int &count, std::vector<std::vector<bool>> &nqueens, const int row,
              const int &n) {
     if (row == n) {
       count++;
@@ -21,9 +21,9 @@ private:
     }
     for (int col = 0; col < n; col++) {
       if (is_valid(nqueens, row, col, n)) {
-        nqueens.at(row).at(col) = 'Q';
+        nqueens.at(row).at(col) = true;
         solve(count, nqueens, row + 1, n);
-        nqueens.at(row).at(col) = '.';
+        nqueens.at(row).at(col) = false;
       }
     }
   }
@@ -31,7 +31,7 @@ private:
 public:
   int totalNQueens(int n) {
     int count = 0;
-    std::vector<std::string> nqueens(n, std::string(n, '.'));
+    std::vector<std::vector<bool>> nqueens(n, std::vector(n, false));
     solve(count, nqueens, 0, n);
     return count;
   }
