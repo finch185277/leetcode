5324d6320a2114d3c38d10d6860d69f816d98cfd
finch185277 <59487andy@gmail.com>
2019-01-26 10:47:23 +0800

    Add 056.cpp

---
+++ 056.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+private:
+  inline static bool sort_intervals(Interval lhs, Interval rhs) {
+    return lhs.start < rhs.start;
+  }
+
+public: // by @lchen77
+  vector<Interval> merge(vector<Interval> &intervals) {
+    std::vector<Interval> ret;
+    if (intervals.empty())
+      return ret;
+    std::sort(intervals.begin(), intervals.end(), sort_intervals);
+    ret.push_back(intervals.at(0));
+    for (int i = 1; i < intervals.size(); i++) {
+      if (ret.back().end < intervals.at(i).start) // they are diff intervals
+        ret.push_back(intervals.at(i));
+      else // enlarge the interval size to cover the overlapping area
+        ret.back().end = std::max(ret.back().end, intervals.at(i).end);
+    }
+    return ret;
+  }
+};
