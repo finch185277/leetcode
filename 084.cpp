aba7fa39264e98ded9f9e29e7cd7157f940acd0e
finch185277 <59487andy@gmail.com>
2019-03-03 14:46:17 +0800

    Update 084.cpp

--- 084.cpp
+++ 084.cpp
@@ -1,19 +1,39 @@
 class Solution {
-public: // by @sipiprotoss5
-  int largestRectangleArea(vector<int> &heights) {
-    int ret = 0;
-    heights.push_back(0);
-    std::vector<int> index;
-    for (int i = 0; i < heights.size(); i++) {
-      while (index.size() > 0 && heights.at(index.back()) > heights.at(i)) {
-        int h = heights.at(index.back());
-        index.pop_back();
-        int sidx = index.size() > 0 ? index.back() : -1;
-        if (h * (i - sidx - 1) > ret)
-          ret = h * (i - sidx - 1);
+private:
+  int max_combine_area(const std::vector<int> &heights, const int lb,
+                       const int mid, const int hb) {
+    int i = mid, j = mid + 1, area = 0,
+        h = std::min(heights.at(i), heights.at(j));
+    while (i >= lb && j <= hb) {
+      h = std::min(h, std::min(heights.at(i), heights.at(j)));
+      area = std::max(area, (j - i + 1) * h);
+      if (i == lb)
+        j++;
+      else if (j == hb)
+        i--;
+      else {
+        if (heights.at(i - 1) > heights.at(j + 1))
+          i--;
+        else
+          j++;
       }
-      index.push_back(i);
     }
-    return ret;
+    return area;
+  }
+  int max_area(const std::vector<int> &heights, const int lb, const int hb) {
+    if (lb == hb)
+      return heights.at(lb);
+    int mid = lb + (hb - lb) / 2;
+    int area = std::max(
+        max_combine_area(heights, lb, mid, hb),
+        std::max(max_area(heights, lb, mid), max_area(heights, mid + 1, hb)));
+    return area;
+  }
+
+public: // by @gildorwang
+  int largestRectangleArea(vector<int> &heights) {
+    if (heights.empty())
+      return 0;
+    return max_area(heights, 0, heights.size() - 1);
   }
 };
