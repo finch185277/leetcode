cd3c1f6136c91910be99bc583ae58bf39664ea3b
finch185277 <59487andy@gmail.com>
2019-03-04 17:02:04 +0800

    Add 094.cpp

---
+++ 094.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+  inline void helper(TreeNode *root, std::vector<int> &ret) {
+    if (root->left != nullptr)
+      helper(root->left, ret);
+    ret.push_back(root->val);
+    if (root->right != nullptr)
+      helper(root->right, ret);
+  }
+
+public: // by Approach 1: Recursive Approach
+  vector<int> inorderTraversal(TreeNode *root) {
+    std::vector<int> ret;
+    if (root != nullptr)
+      helper(root, ret);
+    return ret;
+  }
+};
