d0bcf562ab38e61b2f878204ef0a96c4d108166e
finch185277 <59487andy@gmail.com>
2019-03-04 17:12:08 +0800

    Update 094.cpp

--- 094.cpp
+++ 094.cpp
@@ -10,18 +10,19 @@
 class Solution {
 private:
   inline void helper(TreeNode *root, std::vector<int> &ret) {
-    if (root->left != nullptr)
-      helper(root->left, ret);
-    ret.push_back(root->val);
-    if (root->right != nullptr)
-      helper(root->right, ret);
+    if (root != nullptr) {
+      if (root->left != nullptr)
+        helper(root->left, ret);
+      ret.push_back(root->val);
+      if (root->right != nullptr)
+        helper(root->right, ret);
+    }
   }
 
 public: // by Approach 1: Recursive Approach
   vector<int> inorderTraversal(TreeNode *root) {
     std::vector<int> ret;
-    if (root != nullptr)
-      helper(root, ret);
+    helper(root, ret);
     return ret;
   }
 };
