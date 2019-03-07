0f80d2ff57b513f3484592725c2821f3a6dea39a
finch185277 <59487andy@gmail.com>
2019-03-07 23:42:09 +0800

    Update 097.cpp

--- 097.cpp
+++ 097.cpp
@@ -1,22 +1,23 @@
 class Solution {
-public: // by Approach 4: Using 1D Dynamic Programming
+public: // Approach 3: Using 2D Dynamic Programming
   bool isInterleave(string s1, string s2, string s3) {
     if (s3.size() != s1.size() + s2.size())
       return false;
-    std::vector<bool> dp(s2.size() + 1);
+    std::vector<std::vector<bool>> dp(s1.size() + 1,
+                                      std::vector<bool>(s2.size() + 1));
     for (int i = 0; i <= s1.size(); i++) {
       for (int j = 0; j <= s2.size(); j++) {
         if (i == 0 && j == 0)
-          dp.at(j) = true;
+          dp.at(i).at(j) = true;
         else if (i == 0)
-          dp.at(j) = dp.at(j - 1) && s2[j - 1] == s3[i + j - 1];
+          dp.at(i).at(j) = dp.at(i).at(j - 1) && s2[j - 1] == s3[i + j - 1];
         else if (j == 0)
-          dp.at(j) = dp.at(j) && s1[i - 1] == s3[i + j - 1];
+          dp.at(i).at(j) = dp.at(i - 1).at(j) && s1[i - 1] == s3[i + j - 1];
         else
-          dp.at(j) = (dp.at(j) && s1[i - 1] == s3[i + j - 1] ||
-                      dp.at(j - 1) && s2[j - 1] == s3[i + j - 1]);
+          dp.at(i).at(j) = (dp.at(i - 1).at(j) && s1[i - 1] == s3[i + j - 1] ||
+                            dp.at(i).at(j - 1) && s2[j - 1] == s3[i + j - 1]);
       }
     }
-    return dp.back();
+    return dp.back().back();
   }
 };
