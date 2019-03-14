3ae9c386b57fa1364fcc73c3e9385370dd45a94c
finch185277 <59487andy@gmail.com>
2019-03-14 18:18:43 +0800

    Add 104.cpp

---
+++ 104.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public: // by @ray050899
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr)
+      return 0;
+    return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+  }
+};
