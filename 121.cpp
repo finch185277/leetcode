453328742eab235f6ee1ded4a58f4ad45bf5b64c
finch185277 <59487andy@gmail.com>
2019-03-31 10:22:20 +0800

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -2,11 +2,11 @@ class Solution {
 public: // by Approach 2: One Pass
   int maxProfit(std::vector<int> &prices) {
     int min = std::numeric_limits<int>::max(), ret = 0;
-    for (int i = 0; i < prices.size(); i++) {
-      if (prices.at(i) < min)
-        min = prices.at(i);
-      else if (prices.at(i) - min > ret)
-        ret = prices.at(i) - min;
+    for (int cur : prices) {
+      if (cur < min)
+        min = cur;
+      else if (cur - min > ret)
+        ret = cur - min;
     }
     return ret;
   }
