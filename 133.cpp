ac105837a61b562c3025ee37638c8cf643778b46
finch185277 <59487andy@gmail.com>
2019-04-12 17:57:30 +0800

    Add 133.cpp

---
+++ 133.cpp
@@ -0,0 +1,31 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+private:
+  std::unordered_map<Node *, Node *> copies;
+
+public: // by @jianchao-li
+  Node *cloneGraph(Node *node) {
+    if (!node)
+      return nullptr;
+    if (copies.find(node) == copies.end()) {
+      copies[node] = new Node(node->val, {});
+      for (Node *neighbor : node->neighbors)
+        copies[node]->neighbors.push_back(cloneGraph(neighbor));
+    }
+    return copies[node];
+  }
+};
